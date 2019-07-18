96ad96c75d635765b51aac120e689f0780898f98
dragon434 <dragon434@sina.com>
2019-07-18 22:41:31 +0800

    del .idea dir

--- python学习/集合set.py
+++ python学习/集合set.py
@@ -18,20 +18,26 @@
 a = set([4, 5])
 b = set([4, 5, 6, 7, 8])
 
+print('集合a：', a)
+print('集合b: ', b)
+
 print('交集', a.intersection(b))
 print('交集&', a & b)
 
 print('并集', a.union(b))
 print('并集|', a | b)
 
-print('差集', a.difference(b))
-print('差集-', a - b)
+print('差集a-b', a.difference(b))
+print('差集a-b', a - b)
 
-print('差集', b.difference(a))
-print('差集-', b - a)
+print('差集b-a', b.difference(a))
+print('差集b-a', b - a)
 
 print('对称差集', a.symmetric_difference(b))  # 除了相同的，其他的取出来
 print('对称差集^', a ^ b)
+
+print('对称差集', b.symmetric_difference(a))  # 除了相同的，其他的取出来
+print('对称差集^', b ^ a)
 
 print('子集', a.issubset(b))  # a 是否是 b 的子集
 print('子集 <', a < b)
